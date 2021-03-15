e2969e6b7675ea2119ec95bca860ed948fab53e5
anon <anon@example.com>
2021-03-15 18:49:36 +0100

    Init dev environment.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./scss/style.scss";
+
+
+const App: React.FC = ()  => {
+  return (
+    <div className="bg-primary">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
